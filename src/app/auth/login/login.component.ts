b1fd7e7a9478248446de6cfc51bd49118bfde099
Poleszak <poleszak@protonmail.com>
2022-01-30 17:01:46 +0100

    Login notification

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { throwError } from 'rxjs';
 import { AuthService } from '../shared/auth.service';
 import { LoginRequestPayload } from './login.request.payload';
 
@@ -12,19 +15,31 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   loginRequestPayload: LoginRequestPayload;
+  registerSuccessMessage: string;
+  isError: boolean;
 
-  constructor(private authService: AuthService) { 
+  constructor(private authService: AuthService, private activatedRoute: ActivatedRoute,
+    private router: Router, private toastr: ToastrService) { 
     this.loginRequestPayload = {
       username: '',
       password: ''
     };
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.loginForm = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
     });
+
+    this.activatedRoute.queryParams
+      .subscribe(params => {
+        if (params['registered'] !== undefined && params['registered'] === 'true') {
+          this.toastr.success('Signup Successful');
+          this.registerSuccessMessage = 'Please Check your inbox for activation email '
+            + 'activate your account before you Login!';
+        }
+      });
   }
 
   login() {
@@ -32,7 +47,12 @@ export class LoginComponent implements OnInit {
     this.loginRequestPayload.password = this.loginForm.get('password')?.value;
 
     this.authService.login(this.loginRequestPayload).subscribe(data => {
-      console.log('Login successful');
-    })
+      this.isError = false;
+      this.router.navigateByUrl('');
+      this.toastr.success('Login Successful');
+    }, error => {
+      this.isError = true;
+      throwError(error);
+    });
   }
 }
