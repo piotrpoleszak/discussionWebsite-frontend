4600bc03eadeccd4ebc47590504f964851ea7dcb
Poleszak <poleszak@protonmail.com>
2022-02-21 22:57:35 +0100

    Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostModel } from '../shared/post-model';
 import { PostService } from '../shared/post.service';
+import { faArrowUp, faArrowDown, faComments } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,10 @@ import { PostService } from '../shared/post.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  faArrowUp = faArrowUp;
+  faArrowDown = faArrowDown;
+  faComments = faComments;
 
   posts: Array<PostModel> = [];
 
