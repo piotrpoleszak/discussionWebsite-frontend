a84da332eeb63fa712adb0f10188cb4505c787fb
Poleszak <poleszak@protonmail.com>
2022-02-22 23:31:24 +0100

    Post-title

--- src/app/shared/post-title/post-title.component.spec.ts
+++ src/app/shared/post-title/post-title.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PostTitleComponent } from './post-title.component';
 
 describe('PostTitleComponent', () => {
--- src/app/shared/post-title/post-title.component.ts
+++ src/app/shared/post-title/post-title.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { PostService } from '../post.service';
+import { PostModel } from '../post-model';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-post-title',
+  selector: 'app-post-tile',
   templateUrl: './post-title.component.html',
-  styleUrls: ['./post-title.component.css']
+  styleUrls: ['./post-title.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PostTitleComponent implements OnInit {
 
-  constructor() { }
+  faComments = faComments;
+  @Input() posts: PostModel[];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  goToPost(id: number): void {
+    this.router.navigateByUrl('/view-post/' + id);
+  }
 }
