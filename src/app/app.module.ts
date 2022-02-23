6efe6cf76747f5f3ad039aef113333628792d2c9
Poleszak <poleszak@protonmail.com>
2022-02-23 19:33:34 +0100

    Creating new project after crash configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SignUpComponent } from './auth/signup/signup.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './auth/login/login.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxWebstorageModule } from 'ngx-webstorage';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { HomeComponent } from './home/home.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { PostTitleComponent } from './shared/post-title/post-title.component';
-import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
-import { SideBarComponent } from './shared/side-bar/side-bar.component';
-import { SubpostSideBarComponent } from './shared/subpost-side-bar/subpost-side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    SignUpComponent,
-    LoginComponent,
-    HomeComponent,
-    PostTitleComponent,
-    VoteButtonComponent,
-    SideBarComponent,
-    SubpostSideBarComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    NgxWebstorageModule.forRoot(),
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    FontAwesomeModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
