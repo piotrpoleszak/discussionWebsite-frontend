fb755311ebddec74f9cc0f2d012e138ee3ff376a
Poleszak <poleszak@protonmail.com>
2022-02-26 18:19:27 +0100

    Create home components

---
+++ src/app/shared/subpost-side-bar/subpost-side-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subpost-side-bar',
+  templateUrl: './subpost-side-bar.component.html',
+  styleUrls: ['./subpost-side-bar.component.css']
+})
+export class SubpostSideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
